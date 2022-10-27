52fb5f51f579a8a97e83045824f092efffa68ebf
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2022-10-27 16:03:31 +0530

    feat(backend): add routes for signIn

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('landingpage', { title: 'Express' });
 });
 
+router.get('/customerlogin', (req, res, nex) =>{
+  res.render('customerlogin');
+});
+
+router.get('/employeelogin', (req, res, next)=>{
+  res.render('employeelogin');
+});
+
+
 router.all('*', (req, res, next)=>{
   res.render('404error');
 })
