d780eb983b551ae86fd62f24cc1fbd34dd1b2a1a
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2022-10-31 17:34:01 +0530

    feat(backend): add logic for employee creation"

--- backend/controllers/accountcreate.js
+++ backend/controllers/accountcreate.js
@@ -10,23 +10,18 @@ exports.createCustomer = (req, res, next) =>{
     const companyname = req.body.companynamefield;
     const shipaddress = req.body.shippingaddressfield;
 
-    console.log(username, fullname, pass, cpass, mobileno, city, companyname, shipaddress);
-
     if(!username || !fullname || !pass || !cpass || !mobileno || !city || !companyname || !shipaddress){
-        console.log('empty details');
         return res.status(400).render('customersignup', { message: "Incomplete details"});
     } 
     if(pass != cpass){
-        console.log('passwords do not match');
         return res.status(400).render('customersignup', { message: "Passwords Do not match"});
     }
     let query1 = 'INSERT INTO customers VALUES(?, ?, ?);'
     let query2 = 'INSERT INTO accounts VALUES(?, ?, ?, ?, ?);'
 
-    db.beginTransaction(async(err) => {
-        if(err){
-            throw err;
-        }
+    db.beginTransaction((err) => {
+        if(err) throw err;
+
         db.query(query1, [username, companyname, shipaddress], (err, result) =>{
             if(err) db.rollback((err) => {throw err;});
         })
@@ -43,9 +38,43 @@ exports.createCustomer = (req, res, next) =>{
         });
         res.send("done!");
     });
-
 }
 
+// TODO: needs to be tested
 exports.createEmployee = (req, res, next) =>{
-    
+    const username = req.body.usernamefield;
+    const fullname = req.body.fullnamefield;
+    const pass = req.body.passwordfield;
+    const cpass = req.body.confirmpasswordfield;
+    const mobileno = req.body.mobilenofield;
+    const city = req.body.cityfield;
+    const ismgr = req.body.ismgr;
+    const branch = req.body.branch;
+    if(!username || !fullname || !pass || !cpass || !mobileno || !city || !ismgr || !branch){
+        return res.status(400).render('employeesignup', { message: "Incomplete details"});
+    }
+    if(pass !== cpass){
+        return res.status(400).render('employeesignup', { message: "passwords do not match"});
+    }
+    let query1 = 'INSERT INTO employees VALUES(?, ?, ?)'
+    let query2 = 'INSERT INTO accounts VALUES(?, ?, ?, ?, ?)';
+    db.beginTransaction((err) => {
+        if(err) throw err;
+
+        db.query(query1, [username, ismgr, branc], (err, results) => {
+            if(err) db.rollback((err) => {throw err;});
+        });
+        db.query(query2, [username, pass, mobileno, city], (err, results) => {
+            if(err) db.rollback((err) => {throw err;});
+        });
+
+        db.commit((err) => {
+            if(err){
+                db.rollback((err) => {
+                    throw err;
+                });
+            }
+        });
+        res.send("done!");
+    });
 }
