d9d5f2ef7d06dd92f0c65a389dcb5f88276c29a6
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2022-11-01 23:19:31 +0530

    fix(backend): fixed transactions for createcustomer

--- backend/controllers/accountcreate.js
+++ backend/controllers/accountcreate.js
@@ -1,14 +1,14 @@
 const { db } = require('../connectsql.js');
 
-exports.createCustomer = (req, res, next) =>{
-    const username = req.body.usernamefield;
-    const fullname = req.body.fullnamefield;
-    const pass = req.body.passwordfield;
-    const cpass = req.body.confirmpasswordfield;
-    const mobileno = req.body.mobilenumberfield;
-    const city = req.body.cityfield;
-    const companyname = req.body.companynamefield;
-    const shipaddress = req.body.shippingaddressfield;
+exports.createCustomer = async(req, res, next) =>{
+    const username = await req.body.usernamefield;
+    const fullname = await req.body.fullnamefield;
+    const pass = await req.body.passwordfield;
+    const cpass = await req.body.confirmpasswordfield;
+    const mobileno = await req.body.mobilenumberfield;
+    const city = await req.body.cityfield;
+    const companyname = await req.body.companynamefield;
+    const shipaddress = await req.body.shippingaddressfield;
 
     if(!username || !fullname || !pass || !cpass || !mobileno || !city || !companyname || !shipaddress){
         return res.status(400).render('customersignup', { message: "Incomplete details"});
@@ -16,28 +16,32 @@ exports.createCustomer = (req, res, next) =>{
     if(pass != cpass){
         return res.status(400).render('customersignup', { message: "Passwords Do not match"});
     }
-    let query1 = 'INSERT INTO customers VALUES(?, ?, ?);'
-    let query2 = 'INSERT INTO accounts VALUES(?, ?, ?, ?, ?);'
 
-    db.beginTransaction((err) => {
-        if(err) throw err;
+    let query1 = 'INSERT INTO accounts VALUES(?, ?, ?, ?, ?);'
+    let query2 = 'INSERT INTO customers(cust_id, company, shipping_address) VALUES(?, ?, ?);'
 
-        db.query(query1, [username, companyname, shipaddress], (err, result) =>{
-            if(err) db.rollback((err) => {throw err;});
-        })
-        db.query(query2, [username, pass, fullname, mobileno, city], (err, result) =>{
-            if(err) db.rollback((err) => {throw err;});
-        });
-
-        db.commit((err) => {
-            if(err){
-                db.rollback(() => {
-                    throw err;
+    try{
+        db.beginTransaction(err => {
+            if(err){ throw err; }
+            db.query(query1, [username, pass, fullname, mobileno, city], (err, results) => {
+                if(err){
+                    return db.rollback(() => {throw err;});
+                }
+                db.query(query2, [username, companyname, shipaddress], (err, results) =>{
+                    if(err){
+                        return db.rollback(() => {throw err;});
+                    }
+                    return db.commit( err => {
+                        return db.rollback(() => {throw err;});
+                    });
                 });
-            }
+            });
         });
-        res.send("done!");
-    });
+    } catch(err){
+        console.log('error during transaction - customer creation: ' + err);
+        return res.status.render('500serverissue');
+    }
+    return res.status(200).redirect('/');
 }
 
 // TODO: needs to be tested
@@ -58,18 +62,18 @@ exports.createEmployee = (req, res, next) =>{
     if(pass !== cpass){
         return res.status(400).render('employeesignup', { message: "passwords do not match"});
     }
-    let query1 = 'INSERT INTO employees(emp_id, is_mgr, branch) VALUES(?, ?, ?)'
-    let query2 = 'INSERT INTO accounts VALUES(?, ?, ?, ?, ?)';
+    let query1 ='INSERT INTO accounts VALUES(?, ?, ?, ?, ?)';
+    let query2 = 'INSERT INTO employees("emp_id", "is_mgr", "branch") VALUES(?, ?, ?)';
     db.beginTransaction((err) => {
         if(err) throw err;
 
-        db.query(query1, [username, ismgr, branch], (err, results) => {
+        db.query(query1, [username, pass, mobileno, city,], (err, results) => {
             if(err){
                 db.rollback((err) => {throw err;});
                 console.log('error is here: query1');
             }
         }); 
-        db.query(query2, [username, pass, mobileno, city], (err, results) => {
+        db.query(query2, [username, ismgr, branch], (err, results) => {
             if(err){
                 db.rollback((err) => {throw err;});
                 console.log('error is here: query2');
