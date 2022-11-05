f9713e84a2f5d2591f4a407b7f803a6671bb04ef
Aragorn-64 <advaysagarkar@gmail.com>
2022-11-06 02:14:44 +0530

    feat(frontend) : add order listing for cust dashboard

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const privilege = require('../controllers/privilegemanager');
+const { db }  = require('../connectsql.js');
 
 router.get('/dashboard', privilege.isCustomer, (req, res, next) => {
-    res.status(200).render('customerdashboard', {type : req.session.user.type});
+    db.query('SELECT order_id, prod_id, qty, status FROM `orders` WHERE cust_id = ? ORDER BY status ASC, order_id DESC;', [req.session.user.user_id] , (error, results)=>{
+        if(error) throw error;
+
+        res.status(200).render('customerdashboard', {type : req.session.user.type, custOrders : results});
+    });
+    
 });
 
 module.exports = router;
