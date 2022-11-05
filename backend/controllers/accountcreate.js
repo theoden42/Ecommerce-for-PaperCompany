763d7d0a5e2941866cb2563b4fb038e55103b6f4
Divyansh Tripathi <55395696+theoden42@users.noreply.github.com>
2022-11-05 17:27:58 +0530

    feat(backend): add complete employee creation logic

--- backend/controllers/accountcreate.js
+++ backend/controllers/accountcreate.js
@@ -57,43 +57,48 @@ exports.createEmployee = (req, res, next) =>{
     const fullname = req.body.fullnamefield;
     const pass = req.body.passwordfield;
     const cpass = req.body.confirmpasswordfield;
-    const mobileno = req.body.mobilenofield;
+    const mobileno = req.body.mobilenumberfield;
     const city = req.body.cityfield;
     const ismgr = req.body.ismanagerfield;
     const branch = req.body.branchnamefield;
 
     console.log(username, fullname, pass, cpass, mobileno, city, ismgr, branch);
     if(!username || !fullname || !pass || !cpass || !mobileno || !city || !ismgr || !branch){
-        return res.status(400).render('employeesignup', { message: "Incomplete details"});
+        req.session.message = 'Incomplete Details';
+        return res.status(400).redirect('dashboard/emp');
     }
     if(pass !== cpass){
-        return res.status(400).render('employeesignup', { message: "passwords do not match"});
+        req.session.message = 'Password do not match';
+        return res.status(400).redirect('dashboard/emp');
     }
+
     let query1 ='INSERT INTO accounts VALUES(?, ?, ?, ?, ?)';
-    let query2 = 'INSERT INTO employees("emp_id", "is_mgr", "branch") VALUES(?, ?, ?)';
-    db.beginTransaction((err) => {
-        if(err) throw err;
+    let query2 = 'INSERT INTO employees(emp_id, is_mgr, branch ) VALUES(?, ?, ?)';
 
-        db.query(query1, [username, pass, mobileno, city], (err, results) => {
+    db.beginTransaction(err => {
+        if(err){ 
+            db.rollback(() => { console.log(err); });
+            return res.status(500).render('500serverissue');
+        }   
+         db.query(query1, [username, pass, fullname, mobileno, city], (err, results) => {
             if(err){
-                db.rollback((err) => { throw err; });
-                console.log('error is here: query1');
+                db.rollback(() => { console.log(err) });
+                return res.status(500).render('500serverissue');
             }
-        }); 
-        db.query(query2, [username, ismgr, branch], (err, results) => {
-            if(err){
-                db.rollback((err) => { throw err; });
-                console.log('error is here: query2');
-            } 
-        });
-
-        db.commit((err) => {
-            if(err){
-                db.rollback((err) => {
-                    throw err;
+            db.query(query2, [username, ismgr, branch], (err, results) =>{
+                if(err){
+                     db.rollback(() => { console.log});
+                     return res.status(500).render('500serverissue');
+                }
+                return db.commit( err => {
+                    if(err){
+                        db.rollback(() => { console.log(err)});
+                        return res.status(500).render('500serverissue');
+                    }
+                    return res.status(200).redirect('dashboard/emp');
                 });
-            }
+            });
         });
-        res.send("done!");
     });
+
 }
