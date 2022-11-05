b61c3485f57fc145e319c11d008c9b66450df400
Aragorn-64 <advaysagarkar@gmail.com>
2022-11-05 13:07:53 +0530

    complete mananger dashboard frontend styling

--- backend/routes/manager.js
+++ backend/routes/manager.js
@@ -15,12 +15,27 @@ router.post('/customercreate', privilege.isManager, creator.createCustomer);
 router.post('/employeecreate', privilege.isManager, creator.createEmployee);
 
 router.get('/dashboard', privilege.isManager, (req, res, next) => {
+    res.status(200).redirect('/manager/dashboard/cust');
+});
+
+router.get('/dashboard/emp', privilege.isManager, (req, res, next) => {
     db.query('SELECT name, branch, sales, target from accounts JOIN employees ON emp_id = user_id WHERE is_mgr = 0 ORDER BY sales/target;', (error, results) => {
         if(error){
             throw error;
         }
-        res.status(200).render('managerdashboard', {type : req.session.user.type, empPerformance : results});
-    }) 
+        res.status(200).render('managerdashboardemp', {type : req.session.user.type, empPerformance : results});
+    });
+});
+
+router.get('/dashboard/cust', privilege.isManager, (req, res, next) => {
+    // console.log('reached cust dash');
+    db.query('SELECT name, mob_no, company, city FROM accounts JOIN customers ON user_id = cust_id;', (error, results) => {
+        if(error){
+            throw error;
+        }
+        res.status(200).render('managerdashboardcust', {type : req.session.user.type, custDetails : results});
+    });
+    // res.status(200).render('managerdashboardcust', {type : req.session.user.type, custDetails : results});
 });
 
 router.get('/logout', privilege.isManager, (req, res, next) =>{
