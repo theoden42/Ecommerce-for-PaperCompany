30ef19cf4e3bd19b963a04e86e6d827e788fec68
Aragorn-64 <advaysagarkar@gmail.com>
2022-11-06 01:41:44 +0530

    fix(backend) : change /logout links

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const privilege = require('../controllers/privilegemanager');
 
-router.get('/logout', privilege.isCustomer, (req, res, next) =>{
-    res.status(200).redirect('/logout');
-});
-
 router.get('/dashboard', privilege.isCustomer, (req, res, next) => {
     res.status(200).render('customerdashboard', {type : req.session.user.type});
 });
--- backend/routes/employee.js
+++ backend/routes/employee.js
@@ -2,10 +2,6 @@ const express = require('express');
 const router = express.Router();
 const privilege = require('../controllers/privilegemanager');
 
-router.get('/logout', privilege.isEmployee, (req, res, next) =>{
-    res.status(200).redirect('/logout');
-});
-
 router.get('/dashboard', privilege.isEmployee, (req, res, next) => {
     res.status(200).render('employeedashboard', {type : req.session.user.type});
 });
