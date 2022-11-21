9d1a542af9839bae9c604388d4f82b3f01f4528c
Aragorn-64 <advaysagarkar@gmail.com>
2022-11-21 22:40:58 +0530

    fix(backend): cust order listing show correct prod name

--- backend/routes/customer.js
+++ backend/routes/customer.js
@@ -5,7 +5,7 @@ const ordermanager = require('../controllers/ordermanager');
 const { db }  = require('../connectsql.js');
 
 router.get('/dashboard', privilege.isCustomer, (req, res, next) => {
-    db.query('SELECT order_id, prod_id, qty, status FROM `orders` WHERE cust_id = ? ORDER BY status ASC, order_id DESC;', [req.session.user.user_id] , (error, results)=>{
+    db.query('SELECT order_id, name, qty, status FROM `orders` JOIN `products` ON orders.prod_id = products.prod_id WHERE cust_id = ? ORDER BY status ASC, order_id DESC;', [req.session.user.user_id] , (error, results)=>{
         if(error) throw error;
 
         res.status(200).render('customerdashboard', {type : req.session.user.type, custOrders : results});
